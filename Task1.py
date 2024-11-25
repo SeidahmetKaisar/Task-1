e49fa9a750ac0edc5b323f3acce4f90cf9d9cfb1
SeidahmetKaisar <seidahmet.kaisar@gmail.com>
2024-11-25 18:57:53 +0500

    Add files via upload

---
+++ Task1.py
@@ -0,0 +1,9 @@
+x = 5
+while True:
+    humber = int(input("Сан енгіз:"))
+    if humber > x:
+        print("Берилген сан киши")
+    elif humber < x:
+        print("Берилген саннан киши")
+    if humber == x :
+        print("Санды таптың")
